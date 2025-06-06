b10508c9b7c9d99cf801a497c3c5d58a6ab23056
md-alamin2 <mdalamin22671@gmail.com>
2025-06-06 10:07:00 +0600

    added why choose use section

---
+++ src/Components/WhyChose/WhyChose.jsx
@@ -0,0 +1,32 @@
+import React, { use } from "react";
+
+const whyChosePromise = fetch("./WhyChose.json").then((res) => res.json());
+
+const WhyChose = () => {
+  const whyChoseData = use(whyChosePromise);
+  console.log(whyChoseData);
+  return (
+    <div className="w-11/12 lg:container mx-auto mt-30">
+      <h2 className="text-5xl font-bold text-center">Why Choose FreshReminder Tracker?</h2>
+      <p className="text-2xl font-thin text-center mt-5 max-w-2xl mx-auto">
+        Powerful features designed to help you manage your food inventory
+        efficiently
+      </p>
+      <div className="grid gird-cols-1 md:grid-cols-2 gap-8 mt-10">
+        {whyChoseData.map((data, index) => (
+          <div key={index} className="flex items-start gap-5 py-6 px-8 border border-gray-300 rounded-2xl hover:shadow-xl">
+            <div>
+              <img src={data.icon} alt="" />
+            </div>
+            <div>
+              <h3 className="font-bold mb-3">{data.title}</h3>
+              <p>{data.description}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default WhyChose;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,12 +1,16 @@
 import React, { Suspense } from 'react';
 import Slider from '../../Components/Slider/Slider';
 import Loader from '../../Components/Laoder/Loader';
+import WhyChose from '../../Components/WhyChose/WhyChose';
 
 const Home = () => {
     return (
         <div>
             <Suspense fallback={<Loader></Loader>}>
                 <Slider></Slider>
+            </Suspense>
+            <Suspense>
+                <WhyChose></WhyChose>
             </Suspense>
         </div>
     );
