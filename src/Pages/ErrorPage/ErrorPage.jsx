9ba63ad5d5b4fbdeb4296a6621cfe07e239999cb
md-alamin2 <mdalamin22671@gmail.com>
2025-06-15 16:05:43 +0600

    edit error page

--- src/Pages/ErrorPage/ErrorPage.jsx
+++ src/Pages/ErrorPage/ErrorPage.jsx
@@ -5,10 +5,10 @@ import { Link } from "react-router";
 
 const ErrorPage = () => {
   return (
-    <div className="min-h-screen relative">
-      <Lottie animationData={lottie404} style={{ height: 900 }}></Lottie>
-      <div className="absolute top-3/4 left-[calc(1/2*100%-55px)] w-full">
-        <h1 className="text-4xl font-bold relative -left-30">
+    <div className="md:min-h-screen max-w-screen relative overflow-hidden">
+      <Lottie animationData={lottie404} style={{ height: 500 }}></Lottie>
+      <div className="absolute top-3/4 md:top-2/4 left-[calc(1/2*100%-55px)] w-full">
+        <h1 className="text-4xl font-bold relative -left-32 md:-left-30">
           404 Page Not Found!!
         </h1>
         <Link to="/" className="btn bg-[#64b843] mt-5">
